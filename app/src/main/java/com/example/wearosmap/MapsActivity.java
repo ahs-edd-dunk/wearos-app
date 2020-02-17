10a11f1d89d0ea6ba859a02c126f0875227ee03b
Jairo Pava <jairo_pava@ultimatesoftware.com>
2020-02-17 13:57:30 -0500

    support for sound, vibration, orientation

--- app/src/main/java/com/example/wearosmap/MapsActivity.java
+++ app/src/main/java/com/example/wearosmap/MapsActivity.java
@@ -1,5 +1,35 @@
 package com.example.wearosmap;
 
+import android.Manifest;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Vibrator;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.wear.widget.SwipeDismissFrameLayout;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -9,29 +39,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import android.Manifest;
-import android.location.Location;
-import android.os.Bundle;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.wear.widget.SwipeDismissFrameLayout;
-
-import android.os.Looper;
-import android.support.wearable.activity.WearableActivity;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.view.WindowInsets;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-
-public class MapsActivity extends WearableActivity implements OnMapReadyCallback {
+public class MapsActivity extends WearableActivity implements OnMapReadyCallback, SensorEventListener {
 
     /**
      * Map is initialized when it's fully loaded and ready to be used.
@@ -39,8 +56,24 @@ public class MapsActivity extends WearableActivity implements OnMapReadyCallback
      * @see #onMapReady(com.google.android.gms.maps.GoogleMap)
      */
     private GoogleMap mMap;
+    private LatLng careTakerLocation = new LatLng(0, 0);
+    private LatLng currLocationLatLng = new LatLng(0, 0);
+    private float requiredRotation;
 
     private FusedLocationProviderClient locationClient;
+    private RequestQueue requestQueue;
+    private SoundPool soundPool;
+    private int[] sm;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor mangetometer;
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private float[] orientations = new float[3];
+    private float[] mR = new float[9];
+    private boolean accelerometerSet = false;
+    private boolean magnetometerSet = false;
 
     public void onCreate(Bundle savedState) {
         super.onCreate(savedState);
@@ -94,6 +127,69 @@ public class MapsActivity extends WearableActivity implements OnMapReadyCallback
                 (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        this.requestQueue = Volley.newRequestQueue(this);
+
+        loadSounds();
+
+        this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.mangetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, mangetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (event.getRepeatCount() == 0) {
+            if (keyCode == KeyEvent.KEYCODE_STEM_1) {
+                Log.i("MF KEY", "Keycode 1: " + event.toString());
+
+                if((requiredRotation > 0 && requiredRotation < 30) || (requiredRotation > 330)) {
+                    Log.i("ROTATION", "Move Forward");
+                    //vibrateMoveForward();
+                    //playTurnMoveForwardSound();
+                }
+                else if(requiredRotation > 180) {
+                    Log.i("ROTATION", "Turn left");
+                    //vibrateLeft();
+                    //playTurnLeftSound();
+                } else {
+                    Log.i("ROTATION", "Turn right");
+                    //vibrateRight();
+                    //playTurnRightSound();
+                }
+
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long[] vibrationPattern = {0, 500, 500, 500};
+        final int indexInPatternToRepeat = -1;
+        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+    }
+
+    private void loadSounds() {
+        soundPool = new SoundPool.Builder().build();
+        sm = new int[3];
+
+        soundPool.setOnLoadCompleteListener(
+                new SoundPool.OnLoadCompleteListener() {
+                    @Override
+                    public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                        Log.i("SOUND", "Status: " + status);
+                    }
+                }
+        );
+
+        sm[0] = soundPool.load(this, R.raw.turnleft, 1);
+        sm[1] = soundPool.load(this, R.raw.turnright, 1);
+        sm[2] = soundPool.load(this, R.raw.moveforward, 1);
     }
 
     @Override
@@ -112,12 +208,10 @@ public class MapsActivity extends WearableActivity implements OnMapReadyCallback
         ActivityCompat.requestPermissions(
                 this,
                 new String[]{
-                        Manifest.permission.ACCESS_FINE_LOCATION
+                        Manifest.permission.ACCESS_FINE_LOCATION,
                 },
                 1
         );
-
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
         locationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -126,11 +220,39 @@ public class MapsActivity extends WearableActivity implements OnMapReadyCallback
         lr.setFastestInterval(500);
         lr.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+
+        locationClient.getLastLocation()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("APP", "Error getting location: " + e.getMessage());
+                    }
+                })
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.i("APP", "Got Location! " + location);
+                        if(location != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                                    new LatLng(
+                                            location.getLatitude(),
+                                            location.getLongitude()
+                                    )
+                            ));
+                        }
+                    }
+                });
+
+        Log.i("APP", "moved zoom camera");
+
         locationClient.requestLocationUpdates(
                 lr,
                 new LocationCallback(){
                     @Override
                     public void onLocationResult(LocationResult locationResult) {
+                        Log.i("APP", "location call back");
+
                         if(locationResult == null) {
                             Log.i("APP", "Location not found");
                             return;
@@ -138,13 +260,148 @@ public class MapsActivity extends WearableActivity implements OnMapReadyCallback
 
                         Location currLocation = locationResult.getLastLocation();
                         if(currLocation != null) {
-                            LatLng currLocationLatLng = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(currLocationLatLng));
+                            currLocationLatLng = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
+
+                            getCaretakerLocation();
+
                             mMap.clear();
-                            mMap.addMarker(new MarkerOptions().position(currLocationLatLng).title("Current Location"));
+                            mMap.addMarker(
+                                    new MarkerOptions()
+                                            .position(currLocationLatLng)
+                                            .title("Current Location")
+                            );
+                            mMap.addMarker(
+                                    new MarkerOptions()
+                                            .position(careTakerLocation)
+                                            .title("Caretaker Location")
+                                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                            );
+
+                            postVipLocation(currLocation.getLatitude(),currLocation.getLongitude(),0);
                         }
                     }
                 }, Looper.getMainLooper()
         );
+
+        Log.i("APP", "end");
+    }
+
+    private void getCaretakerLocation()
+    {
+        final String url = "https://edd-dunk-server.appspot.com/caretaker";
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Log.i("GET Response", response.toString());
+                        try {
+                            double latitude = response.getDouble("latitude");
+                            double longitude = response.getDouble("longitude");
+                            double bearing = response.getDouble("bearing");
+
+                            careTakerLocation = new LatLng(latitude, longitude);
+
+                            Log.i("GET Response", latitude + "");
+                            Log.i("GET Response", longitude + "");
+                            Log.i("GET Response", bearing + "");
+                        } catch (Exception e) {
+                            Log.i("Error", e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("GET Error.Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(getRequest);
+    }
+
+    private void postVipLocation(double lat, double lon, double bear) {
+        final String url = "https://edd-dunk-server.appspot.com/vip";
+        JSONObject jsonBody = new JSONObject();
+
+        try {
+            jsonBody.put("latitude", lat);
+            jsonBody.put("longitude", lon);
+            jsonBody.put("bearing", bear);
+        } catch (JSONException e) {
+            Log.i("Error", e.getMessage());
+        }
+
+        Log.i("APP", "creating request");
+        JsonObjectRequest postRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.i("GET Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("POST Error.Response", error.toString());
+                    }
+                }
+        );
+
+        Log.i("APP", "posting location");
+
+        requestQueue.add(postRequest);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            accelerometerSet = true;
+        } else if(event.sensor == mangetometer) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            magnetometerSet = true;
+        }
+
+        if(!accelerometerSet || !magnetometerSet) {
+            return;
+        }
+
+        SensorManager.getRotationMatrix(mR, null, lastAccelerometer, lastMagnetometer);
+        SensorManager.getOrientation(mR, orientations);
+
+        float azimuthInRadians = orientations[0];
+        float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+
+        Location currentLocation = new Location("");
+        currentLocation.setLatitude(currLocationLatLng.latitude);
+        currentLocation.setLongitude(currLocationLatLng.longitude);
+        currentLocation.setBearing(azimuthInDegrees);
+
+        Log.i("ROTATION", "Current Rotation: " + azimuthInDegrees);
+
+        Location targetLocation = new Location("");
+        targetLocation.setLatitude(careTakerLocation.latitude);
+        targetLocation.setLongitude(careTakerLocation.longitude);
+
+        Log.i("ROTATION", "Bearing to: " + ((currentLocation.bearingTo(targetLocation) + 360) % 360));
+
+        this.requiredRotation = (360 + ((currentLocation.bearingTo(targetLocation) + 360) % 360) - currentLocation.getBearing()) % 360;
+
+        Log.i("ROTATION", "Required Rotation: " + requiredRotation);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
     }
 }
